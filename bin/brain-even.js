ce81e54b1e36454777dbe140233d8d97a515f831
Dmitry Zvonarev <zvonarev.dmitry@yandex.ru>
2022-03-02 01:36:21 +0300

    Installed lodash and mplemented logic of first game

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,3 +1,28 @@
+import _ from 'lodash';
+import readlineSync from 'readline-sync';
+
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
-const randomCount = Math.random();
-console.log(`Question: ${randomCount}`);
+let i = 0;
+const askQuestion = () => {
+  const randomeCount = _.random(1, 99);
+  if (i === 3) {
+    return console.log('Congratulatin!');
+  }
+  console.log(`Question: ${randomeCount}`);
+  const answerOfQuestion = readlineSync.question('Your answer ');
+  let rightAnswer;
+  if (randomeCount % 2 === 0) {
+    rightAnswer = 'yes';
+  } else {
+    rightAnswer = 'no';
+  }
+  if (answerOfQuestion === rightAnswer) {
+    console.log('Correct!');
+    i += 1;
+    askQuestion();
+  } else {
+    console.log(`"${answerOfQuestion}" is wrong answer ;(. Correct answer was "${rightAnswer}".`);
+  }
+};
+
+askQuestion();
