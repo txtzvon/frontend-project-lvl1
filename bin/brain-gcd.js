ad6fc15f3e3f4739a89669822fe8fcbf48fa2895
Dmitry Zvonarev <zvonarev.dmitry@yandex.ru>
2022-03-12 19:24:35 +0300

    Added shebang

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import largestDivider from '../src/games/gcd.js';
 
 largestDivider();
--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import progressionOfNum from '../src/games/progression.js';
 
 progressionOfNum();
