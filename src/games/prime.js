a4802235dc80a517f029c056ff4eb615099d6232
Dmitry Zvonarev <zvonarev.dmitry@yandex.ru>
2022-03-12 20:04:20 +0300

    Fixed last game

--- src/games/prime.js
+++ src/games/prime.js
@@ -2,25 +2,25 @@ import _ from 'lodash';
 import { greetingUser, finalMessage, levelOfGame } from '../index.js';
 
 const isPrime = (num) => {
-  let flag = 'yes';
-  for (let i = 2; i < num; i += 1) {
+  if (num <= 1) {
+    return false;
+  }
+  for (let i = 2; i <= num / 2; i += 1) {
     if (num % i === 0) {
-      flag = 'no';
+      return false;
     }
   }
-  return flag;
+  return true;
 };
-
 const brainPrime = () => {
   greetingUser();
   console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
-
   let result;
   let correctCounter = 0;
 
   do {
-    const question = _.random(2, 100);
-    const correctAnswer = isPrime(question);
+    const question = _.random(1, 100);
+    const correctAnswer = isPrime(question) ? 'yes' : 'no';
     result = levelOfGame(question, correctAnswer);
     if (result === 'true') {
       correctCounter += 1;
