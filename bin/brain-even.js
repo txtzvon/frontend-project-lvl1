68e6ac60ca4ce5ae2c98d05b09412dc85a35e650
Dmitry Zvonarev <zvonarev.dmitry@yandex.ru>
2022-03-19 17:51:31 +0300

    Implemented brain-even with new logic

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 // eslint-disable-next-line import/no-named-as-default
-import brainEven from '../src/games/even.js';
+import launchBrainEvenGame from '../src/games/even.js';
 
-brainEven();
+launchBrainEvenGame();
--- src/games/even.js
+++ src/games/even.js
@@ -1,23 +1,17 @@
-import {
-  greetingUser, finalMessage, levelOfGame, getRandomNum,
-} from '../index.js';
+import { getRandomNum, engine } from '../index.js';
 
-export default function brainEven() {
-  greetingUser();
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+const brainEvenGame = () => {
+  const instruction = 'Answer "yes" if the number is even, otherwise answer "no".';
 
   const isEven = (num) => ((num % 2 === 0) ? 'yes' : 'no');
 
-  let result;
-  let correctCounter = 0;
-  do {
+  const round = () => {
     const question = getRandomNum(1, 50);
     const correctAnswer = isEven(question);
-    result = levelOfGame(question, correctAnswer);
-    if (result === 'true') {
-      correctCounter += 1;
-    }
-  } while (result === 'true' && correctCounter < 3);
+    return [question, correctAnswer];
+  };
 
-  finalMessage(correctCounter);
-}
+  engine(instruction, round);
+};
+
+export default brainEvenGame;
